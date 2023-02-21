925ed0d50dd01cda3dd74f468536c41e6d64bd79
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-21 12:31:59 +0100

    salvo esercizio sul palindromo

---
+++ eserciziopalindroma.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool palindroma(string parola)
+{
+    bool risultato = true;
+
+    int k = parola.length() - 1;
+
+    for (int i = 0; i < parola.length(); i++)
+    {
+        cout << parola[i] << " == " << parola[k] <<endl ;
+        if (parola[i] == parola[k])
+        {
+            k--;
+        }
+
+        else
+        {
+            return false;
+        } 
+        
+    }  cout << parola << endl;
+        return risultato; }
+
+    int main()
+    {
+        string parola = "yamamay";
+
+        
+        if (palindroma(parola))
+        {
+            cout << "La parola è palindroma" << endl;
+        }
+        else
+        {
+            cout << "La parola non è palindroma" << endl;
+        }
+    }
